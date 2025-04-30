87e0677e2bb56f6536ad05e0b3ce9e31b950078a
Ivaylo Zlatanov <iizlatanov@gmail.com>
2025-04-30 23:48:21 +0300

    Templates Advanced.

--- templates_advancedD/templates_advancedD/web/views.py
+++ templates_advancedD/templates_advancedD/web/views.py
@@ -3,13 +3,22 @@ import random
 from django.shortcuts import render
 
 
-cats = [
+cat_images = (
     'https://cdn.britannica.com/39/226539-050-D21D7721/Portrait-of-a-cat-with-whiskers-visible.jpg',
     'https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg',
-]
+)
+
+cat_names = (
+    'Pepelyashka',
+    'Gosho',
+)
+
+
 def index(request):
+    index = random.randint(0, len(cat_images) - 1)
     context = {
-        'cat_image':random.choice(cats),
+        'cat_image': cat_images[index],
+        'cat_name': cat_names[index],
     }
     return render(request, 'web/index.html', context)
 
